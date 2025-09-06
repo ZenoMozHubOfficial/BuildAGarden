ce21163344a0c6309c3b25d0c0a94b03911cf7f6
ZenoMozHubOfficial <ZenoMozHubOfficial@gmail.com>
2025-09-06 15:14:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Particle starfield
 const canvas = document.getElementById("starfield");
 const ctx = canvas.getContext("2d");
 
+// Particle setup
 let stars = [];
-let numStars = 150;
+let numStars = 150 * 3; // 3x more particles
 
 function resizeCanvas() {
   canvas.width = window.innerWidth;
@@ -18,13 +18,13 @@ for (let i = 0; i < numStars; i++) {
     x: Math.random() * canvas.width,
     y: Math.random() * canvas.height,
     size: Math.random() * 2,
-    speed: Math.random() * 0.5 + 0.2
+    speed: Math.random() * 3 + 2 // very fast
   });
 }
 
 function drawStars() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "white";
+  ctx.fillStyle = "#00BFFF"; // blue particles
   stars.forEach(star => {
     ctx.beginPath();
     ctx.arc(star.x, star.y, star.size, 0, Math.PI * 2);
@@ -40,8 +40,15 @@ function drawStars() {
 }
 drawStars();
 
-// Button click sound
-function playClick() {
+// Button click sound + teleport
+function playClick(url) {
   const audio = new Audio("settings/click.mp3");
   audio.play();
+  if (url) window.open(url, "_blank"); // teleport to link
 }
+
+// Assign buttons to links
+document.querySelector(".btn.discord").addEventListener("click", () => playClick("https://discord.gg/YOUR_LINK"));
+document.querySelector(".btn.youtube").addEventListener("click", () => playClick("https://www.youtube.com/"));
+document.querySelector(".btn.tiktok").addEventListener("click", () => playClick("https://www.tiktok.com/"));
+document.querySelector(".btn.blog").addEventListener("click", () => playClick("https://YOUR_BLOG_LINK"));
